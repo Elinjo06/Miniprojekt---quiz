a00c1ad1bb5f3b3de42efea8a49ac32bec2242b6
Elinjo06 <44798948+Elinjo06@users.noreply.github.com>
2019-02-08 14:39:19 +0100

    2019-02-08

--- Miniprojekt quiz/script.js
+++ Miniprojekt quiz/script.js
@@ -1,4 +1,37 @@
 //Funktion 1
-function kontrol(){
-	alert("Det funkar");
+function kontroll(){
+var correct = 0;
+document.getElementById('tryckt').style.visibility = "visible";
+
+
+	var val1 = document.fragor.val1.value;
+	var val2 = document.fragor.val2.value;
+	var val3 = document.fragor.val3.value
+	var val4 = document.fragor.val4.value;
+	var val5 = document.fragor.val5.value;
+var correct = 0;
+		if (val1 == "blekinge") {
+			correct++;
+		}
+
+		if (val2 == "uppland") {
+			correct++;
+		}
+
+
+		if (val3 == "gastrikland") {
+			correct++;
+		}
+
+		if (val4 == "smaland") {
+			correct++;
+		}
+
+		if (val5 == "oland") {
+			correct++;
+		}
+
+
+		
+document.getElementById('ratantal').innerHTML = "Du har " + correct + " rätt.";
 }
